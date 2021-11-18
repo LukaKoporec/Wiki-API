1c04c321df574e4e8e3d1cc8a8a1757c93442392
LukaKoporec <luka.koporec123@gmail.com>
2021-11-18 10:11:47 +0100

    GET, POST, DELETE

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = "express";
-const bodyParser = "body-parser";
-const mongoose = "mongoose";
-const ejs = "ejs";
+const express = require ("express");
+const bodyParser = require ("body-parser");
+const mongoose = require ("mongoose");
+const ejs = require ("ejs");
 
 const app = express();
 
@@ -18,6 +18,44 @@ const articleSchema = {
 };
 
 const Article = mongoose.model("Article", articleSchema);
+
+app.get("/articles", function(req, res){
+  Article.find(function(err, foundArticles){
+    if (!err) {
+      res.send(foundArticles);
+    } else {
+      res.send(err);
+    }
+    
+  });
+});
+
+app.post("/articles", function(req, res){
+  console.log(req.body.title);
+  console.log(req.body.content);
+
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+  });
+  newArticle.save(function(err){
+    if (!err) {
+      res.send("Successfully added a new article.");      
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.delete("/articles", function(req, res){
+  Article.deleteMany(function(err){
+    if (!err) {
+      res.send("Successfully deleted all articles.")
+    } else {
+      res.send(err);      
+    }
+  });
+});
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
